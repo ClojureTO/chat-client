d29af8d5dbe124bd81d5953e83945212f5f53412
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-03-21 15:55:01 -0400

    committed client code

--- src/chat_client/core.clj
+++ src/chat_client/core.clj
@@ -1,6 +1,26 @@
-(ns chat-client.core)
+(ns chat-client.core
+  (:require [clj-sockets.core :as socket]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def conn (atom nil))
+
+(defn close-connetion! []
+  (when-let [c @conn]
+    (socket/close-socket c)
+    (reset! conn nil)))
+
+(defn connect-user! [id]
+  (when-let [c @conn]
+    (close-connetion!))
+  (reset! conn (socket/create-socket "localhost" 1234))
+  (socket/write-line @conn (str "USER " id)))
+
+(defn send-message [message]
+  (if-let [c @conn]
+    (socket/write-line c (str "MSG " message))
+    (throw (Exception. "no connection available!"))))
+
+;(connect-user! "Foo")
+
+;(send-message "heeeelloooooo")
+
+;(close-connetion!)
