bd66cf12b99d08b412c4c40fe3264a279e02891a
J Irving <j@lollyshouse.ca>
2015-03-29 18:08:25 -0400

    Bot works, ditch sockets lib

--- src/chat_client/core.clj
+++ src/chat_client/core.clj
@@ -1,38 +1,49 @@
 (ns chat-client.core
-  (:require [clj-sockets.core :as socket]))
+  (:require [clojure.java.io :as io])
+  (:import (java.net Socket SocketException)))
 
 (def conn (atom nil))
 
 (defn close-connetion! []
   (when-let [c @conn]
-    (socket/close-socket c)
+    (.close (:socket c))
     (reset! conn nil)))
+
+(defn send-message [message]
+  (if-let [c @conn]
+    (doto (:writer c)
+      (.write (str "MSG " message "\n"))
+      (.flush))
+    (throw (ex-info "Client not connected" {:failed-message message}))))
 
 (defn connect-user! [id]
   (when-let [c @conn]
     (close-connetion!))
-  (reset! conn (socket/create-socket "localhost" 1234))
-  (socket/write-line @conn (str "USER " id)))
-
-(defn send-message [message]
-  (if-let [c @conn]
-    (socket/write-line c (str "MSG " message))
-    (throw (Exception. "no connection available!"))))
+  (let [socket (Socket. "localhost" 1234)
+        reader (io/reader socket)
+        writer (io/writer socket)]
+    (reset! conn {:socket socket :reader reader :writer writer})
+    (doto writer
+      (.write (str "USER " id "\n"))
+      (.flush))))
 
 ;; Example bot
 (defn hello-bot []
   (connect-user! "hello-bot")
   (send-message "hi there")
-  (loop [msg (socket/read-lines @conn)]
-    (when (re-matches #".*hello.*" (first msg))
-      (send-message "heeeeelllllloooooo back!"))
-    ;; if you want to kill the function, do this:
-    ;; (reset! chat-client.core/conn nil)
-    (when @conn
-      (recur (socket/read-line @conn)))))
+  (future
+    (try
+      (loop [msg (.readLine (:reader @conn))]
+        (when (nil? msg)   ; readLine returns nil when the connection closes
+          (reset! conn nil))
 
-;(connect-user! "Foo")
-
-;(send-message "heeeelloooooo")
-
-;(close-connetion!)
+        (when-let [nick (some-> msg
+                                (and (re-matches #"^(.*):.*hello.*" msg))
+                                second)]
+          (send-message (str "heeeeelllllloooooo " nick "!")))
+        ;; if you want to terminate the future and kill the function,
+        ;; do this: (reset! chat-client.core/conn nil)
+        (when @conn
+          (recur (.readLine (:reader @conn)))))
+      (catch Throwable e
+        (println (.getMessage e) "sending" (-> e ex-data :failed-message))))))
