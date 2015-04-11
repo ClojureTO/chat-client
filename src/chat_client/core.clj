ea0811f8d262e7b2b15204d692f9b3d38faf7015
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-04-11 13:41:18 -0400

    added listing clients

--- src/chat_client/core.clj
+++ src/chat_client/core.clj
@@ -3,14 +3,13 @@
 
 (def conn (atom nil))
 
-(defn close-connetion! []
+(defn close-connetion! [conn]
   (when-let [c @conn]
     (socket/close-socket c)
     (reset! conn nil)))
 
 (defn connect-user! [id]
-  (when-let [c @conn]
-    (close-connetion!))
+  (close-connetion! conn)
   (reset! conn (socket/create-socket "localhost" 1234))
   (socket/write-line @conn (str "USER " id)))
 
@@ -19,8 +18,14 @@
     (socket/write-line c (str "MSG " message))
     (throw (Exception. "no connection available!"))))
 
+(defn list-clients []
+  (if-let [c @conn]
+    (do
+      (socket/write-line c "LIST")
+      (println (socket/read-line c)))
+    (throw (Exception. "no connection available!"))))
+
 ;(connect-user! "Foo")
-
 ;(send-message "heeeelloooooo")
-
+;(list-clients)
 ;(close-connetion!)
